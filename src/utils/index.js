4fb31098f7dc593bc03890205d5e807b1f15ec3a
zdndd <dongningdella@gmail.com>
2022-10-26 17:48:20 +0800

    3-4 useMount没有起效果，useDebounce是有效果的

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (value) => (value === 0 ? false : !value);
 export const cleanObject = (object) => {
   const result = { ...object };
@@ -8,4 +10,19 @@ export const cleanObject = (object) => {
     }
   });
   return result;
+};
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export const useDebounce = (value, delay) => {
+  const [debounceValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debounceValue;
 };
