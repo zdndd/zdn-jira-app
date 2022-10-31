6711aae78f57cf25ae704b372a26a1545a7ae34a
zdndd <dongningdella@gmail.com>
2022-10-31 17:23:21 +0800

    4-2

--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,23 +1,49 @@
-export const SearchPanel = ({param,setParam,users}) => {
-    
-    return <div>
-       
-        <form>
-        
-        <input type="text"  value={param.name} onChange={evt => setParam({
-            ...param,
-            name:evt.target.value
-        })}/>
-        <select value = {param.personId} onChange={evt => setParam({
-            ...param,
-            personId:evt.target.value
-        })}>
-            <option value={''}>负责人</option>
-            {
-                users.map(user =><option value={user.id}>{user.name}</option>)
-            }
-        </select>
-    </form>
-    </div>
-   
+interface SearchPanelProps {
+  users: User[];
+  param: {
+    name: string;
+    personId: string;
+  };
+  setParam: (param: SearchPanelProps["param"]) => void;
 }
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  title: string;
+  organization: string;
+}
+
+export const SearchPanel = ({ param, setParam, users }: SearchPanelProps) => {
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          value={param.name}
+          onChange={(evt) =>
+            setParam({
+              ...param,
+              name: evt.target.value,
+            })
+          }
+        />
+        <select
+          value={param.personId}
+          onChange={(evt) =>
+            setParam({
+              ...param,
+              personId: evt.target.value,
+            })
+          }
+        >
+          <option value={""}>负责人</option>
+          {users.map((user) => (
+            <option value={user.id}>{user.name}</option>
+          ))}
+        </select>
+      </form>
+    </div>
+  );
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,24 +1,27 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value) => (value === 0 ? false : !value);
-export const cleanObject = (object) => {
+export const isFalsy = (value: any) => (value === 0 ? false : !value);
+export const cleanObject = (object: object) => {
   const result = { ...object };
   Object.keys(result).forEach((key) => {
+    // 涉及到范型
+    // @ts-ignore
     const value = result[key];
     if (isFalsy(value)) {
+      // @ts-ignore
       delete result[key];
     }
   });
   return result;
 };
 
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
   }, []);
 };
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: any, delay?: number) => {
   const [debounceValue, setDebouncedValue] = useState(value);
   useEffect(() => {
     const timeout = setTimeout(() => setDebouncedValue(value), delay);
