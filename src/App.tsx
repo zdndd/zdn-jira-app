bd46568033116c4d4f71f572bb6d33d7b1befb9a
zdndd <dongningdella@gmail.com>
2022-11-08 17:05:27 +0800

    5-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { ProjectListScreen } from "./screens/project-list";
+import { LoginScreen } from "./screens/login";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen></ProjectListScreen>
+      {/* <ProjectListScreen></ProjectListScreen> */}
+      <LoginScreen></LoginScreen>
     </div>
   );
 }
---
+++ src/screens/login/index.tsx
@@ -0,0 +1,37 @@
+import { FormEvent } from "react";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+export const LoginScreen = () => {
+  const login = (param: { username: string; password: string }) => {
+    fetch(`http://localhost:3001/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(param),
+    }).then(async (response) => {
+      if (response.ok) {
+      }
+    });
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const username = (event.currentTarget.elements[0] as HTMLFormElement).value;
+    const password = (event.currentTarget.elements[1] as HTMLFormElement).value;
+    login({ username, password });
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">用户名</label>
+        <input type="text" id={"username"}></input>
+      </div>
+      <div>
+        <label htmlFor="password">密码</label>
+        <input type="password" id={"password"}></input>
+      </div>
+      <button type={"submit"}>登录</button>
+    </form>
+  );
+};
